a0dd0209c7c41c0973a3309fd1303b9c9a83126a
Steven Walter <stevenrwalter@gmail.com>
2023-06-07 18:10:09 -0400

    Make completed a date instead of a bool

--- procedures.sql
+++ procedures.sql
@@ -5,8 +5,8 @@ CREATE PROCEDURE createTodo (title VARCHAR(512), focus tinyint(1),
     state ENUM('Next', 'Later', 'Waiting', 'Someday', 'Archive'),
     due DATE, scheduled DATE, recurringDays INT, details TEXT, OUT todoID INT)
 BEGIN
-    INSERT INTO todo (sort, complete, title, focus, state, due, scheduled, recurringDays, details)
-        VALUES (0, 0, title, focus, state, due, scheduled, recurringDays, details);
+    INSERT INTO todo (sort, title, focus, state, due, scheduled, recurringDays, details)
+        VALUES (0, title, focus, state, due, scheduled, recurringDays, details);
     SET todoID = LAST_INSERT_ID();
     UPDATE todo SET sort = todoID * 10 WHERE id = todoID;
 END //
@@ -14,7 +14,7 @@ END //
 DROP PROCEDURE IF EXISTS listTodos //
 CREATE PROCEDURE listTodos ()
 BEGIN
-    SELECT id AS _id, complete, title, state, due
+    SELECT id AS _id, completed, title, state, due
         FROM todo ORDER BY sort;
 END //
 
@@ -28,7 +28,7 @@ DROP PROCEDURE IF EXISTS displayTodo //
 CREATE PROCEDURE displayTodo (_id INT)
 BEGIN
     SELECT title FROM todo WHERE todo.id = _id;
-    SELECT complete FROM todo WHERE todo.id = _id;
+    SELECT completed FROM todo WHERE todo.id = _id;
     SELECT focus FROM todo WHERE todo.id = _id;
     SELECT state FROM todo WHERE todo.id = _id;
     SELECT due FROM todo WHERE todo.id = _id;
@@ -39,13 +39,13 @@ BEGIN
 END //
 
 DROP PROCEDURE IF EXISTS modifyTodo //
-CREATE PROCEDURE modifyTodo (_id INT, title VARCHAR(512), complete tinyint(1), focus tinyint(1),
+CREATE PROCEDURE modifyTodo (_id INT, title VARCHAR(512), completed DATE, focus tinyint(1),
     state ENUM('Next', 'Later', 'Waiting', 'Someday', 'Archive'),
     due DATE, scheduled DATE, recurringDays INT, details TEXT, OUT result VARCHAR(255))
 BEGIN
     UPDATE todo AS t SET
         t.title = title,
-        t.complete = complete,
+        t.completed = completed,
         t.focus = focus,
         t.state = state,
         t.due = due,
@@ -59,8 +59,8 @@ END //
 DROP PROCEDURE IF EXISTS quickEntry //
 CREATE PROCEDURE quickEntry (title VARCHAR(512), OUT todoID INT)
 BEGIN
-    INSERT INTO todo (sort, complete, title)
-        VALUES (0, 0, title);
+    INSERT INTO todo (sort, title)
+        VALUES (0, title);
     SET todoID = LAST_INSERT_ID();
     UPDATE todo SET sort = todoID * 10 WHERE id = todoID;
 END //
